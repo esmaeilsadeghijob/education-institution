d74cb0bce98f46979c21f45b386df623928d8e7a
فرزین پور عبداللهی <02040201@Candocollege.local>
2023-11-20 12:31:53 +0330

    Modified UserRepository

--- src/main/java/com/cando/uni/repository/UserRepository.java
+++ src/main/java/com/cando/uni/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.cando.uni.repository;
 
+import antlr.collections.List;
 import com.cando.uni.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<List<User>> findByEmail (String email);
 }
