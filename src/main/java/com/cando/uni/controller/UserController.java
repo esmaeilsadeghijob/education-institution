2c36d64539385502b24d3366c1bd27c790b158a2
itccc-ir <itccc.ir.ir@gmail.com>
2023-11-26 20:46:25 +0330

    modified Update and findbyUsername and findByEmail

---
+++ src/main/java/com/cando/uni/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.cando.uni.controller;
+
+import com.cando.uni.model.User;
+import com.cando.uni.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> users() {
+        return userService.users();
+    }
+
+    @GetMapping("/{id}")
+    public User findUserById(@PathVariable Long id) {
+        return userService.findUserById(id);
+    }
+
+    @PostMapping
+    public User save(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        userService.delete(id);
+    }
+
+    @PutMapping()
+    public void update(@RequestBody User user)
+    {
+        userService.update(user);
+    }
+
+    @GetMapping("/{email}")
+    public List<User> findUserByEmail(@PathVariable String email) {
+        return userService.findUserByEmail(email);
+    }
+
+    @GetMapping("/{username}")
+    public List<User> findUserByUsername(@PathVariable String username) {
+        return userService.findUserByUsername(username);
+    }
+
+}
