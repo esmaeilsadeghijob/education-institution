a39a1ca5bbba3c41c5742d7f145a4b88dfd33dcb
itccc-ir <itccc.ir.ir@gmail.com>
2023-11-27 09:28:20 +0330

    change project

---
+++ src/main/java/com/cando/uni/model/User.java
@@ -0,0 +1,19 @@
+package com.cando.uni.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+}
