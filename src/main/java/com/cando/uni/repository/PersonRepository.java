5a5a2e161d694785046f0ad3332259c7dc20b963
وحید رستمی <02040198@Candocollege.local>
2023-11-27 12:21:34 +0330

    vahiddchange

--- src/main/java/com/cando/uni/repository/PersonRepository.java
+++ src/main/java/com/cando/uni/repository/PersonRepository.java
@@ -2,8 +2,16 @@ package com.cando.uni.repository;
 
 import com.cando.uni.model.Person;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import javax.annotation.Resource;
 
 @RepositoryRestResource (collectionResourceRel = "person",path = "person")
 public interface PersonRepository  extends PagingAndSortingRepository<Person,Long> {
+@RestResource(rel ="findbyusername",path = "findbyusername")
+    Person findByusername(@Param("UserName") String username);
+
+
 }
