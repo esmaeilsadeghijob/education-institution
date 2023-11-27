dd43de8924c33f3c3ff5cce2cec230f563cf02bc
AlizadehHamid <alizadeh.hamid.h@gmail.com>
2023-11-27 12:24:22 -0800

    search by fname

--- src/main/java/com/cando/uni/repository/PersonRepository.java
+++ src/main/java/com/cando/uni/repository/PersonRepository.java
@@ -2,8 +2,12 @@ package com.cando.uni.repository;
 
 import com.cando.uni.model.person;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(collectionResourceRel = "Person",path="Person")
 public interface PersonRepository extends PagingAndSortingRepository<person,Long> {
-}
+    @RestResource(path="FindByfname",rel = "FindByfname")
+    person FindByfname(@Param("fname") String fname);
+};
