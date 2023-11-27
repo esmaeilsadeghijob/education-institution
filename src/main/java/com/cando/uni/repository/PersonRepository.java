54cdee60d1dbd2028a71103824aa67d520636e96
behroozfallah70 <behroozfallah70@gmail.com>
2023-11-27 12:22:51 +0330

    Three Commit Spring data

--- src/main/java/com/cando/uni/repository/PersonRepository.java
+++ src/main/java/com/cando/uni/repository/PersonRepository.java
@@ -2,8 +2,17 @@ package com.cando.uni.repository;
 
 import com.cando.uni.model.Person;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(collectionResourceRel = "person",path = "person")
 public interface PersonRepository extends PagingAndSortingRepository<Person,Long> {
+@RestResource(path = "serachByFirstname",rel = "serachByFirstname")
+    Person findByFirstname(@Param("firstname") String firstname);
+    @RestResource(path = "serachByLastname",rel = "serachByLastname")
+    Person findByLastname(@Param("lastname") String lastname);
+
 }
+
+
