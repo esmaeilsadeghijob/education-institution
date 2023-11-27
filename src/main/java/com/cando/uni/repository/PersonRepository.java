1b67f27f8c26258cefd09c2250b54cc19a57a47a
itccc-ir <itccc.ir.ir@gmail.com>
2023-11-27 12:20:15 +0330

    add findByname and address by MRahi

--- src/main/java/com/cando/uni/repository/PersonRepository.java
+++ src/main/java/com/cando/uni/repository/PersonRepository.java
@@ -2,9 +2,15 @@ package com.cando.uni.repository;
 
 import com.cando.uni.model.Person;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 @RepositoryRestResource(collectionResourceRel="Preson",path="person")
 public interface PersonRepository extends PagingAndSortingRepository<Person,Long> {
+    @RestResource(rel="findByName",path="findByName")
+    Person findByName(@Param("name") String name);
+    @RestResource(rel="findByAddress",path="findByAddress")
+    Person findByAddress(@Param("address") String address);
 }
