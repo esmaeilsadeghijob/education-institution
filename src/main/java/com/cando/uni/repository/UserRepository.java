fab964a4b9e8ae01d581cfcdbfc1bd608178b134
FarzinPourabdolahi <Pourabdolahi.Farzin@gmail.com>
2023-12-03 19:52:59 +0330

    @RepositoryRestResource

--- src/main/java/com/cando/uni/repository/UserRepository.java
+++ src/main/java/com/cando/uni/repository/UserRepository.java
@@ -6,9 +6,13 @@ import java.util.Optional;
 import com.cando.uni.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<List<User>> findByEmail (String email);
+
+    @RestResource(rel="searchbyuserId", path ="searchbyuserId" )
+    User findByUserId ()
 }
